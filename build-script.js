526c9e91c3c05d38001d4932e986161af7fb3b8b
Mikael Grön <skaramicke@users.noreply.github.com>
2023-10-28 11:13:17 +0200

    Update build-script.js

--- build-script.js
+++ build-script.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // Step 1: Read events from text file
 const eventsTxt = fs.readFileSync(path.resolve(__dirname, 'events.txt'), 'utf-8');
-const events = eventsTxt.split('\n').map(line => {
+const events = eventsTxt.trim().split('\n').map(line => {
   const [title, date] = line.split(',');
   return { title, date };
 });
@@ -11,7 +11,7 @@ const events = eventsTxt.split('\n').map(line => {
 // Step 2: Generate HTML content dynamically
 let eventDivs = '';
 events.forEach(event => {
-  eventDivs += `<div class="event-box" id="${event.title}">${event.title}: </div>\n`;
+  eventDivs += `<div class="event-box"><span>${event.title}: </span><span id="${event.title}"></span></div>\n`;
 });
 
 // Inject the event divs into index.html
@@ -22,6 +22,6 @@ fs.writeFileSync(path.resolve(__dirname, 'index.html'), indexHtml);
 
 // Step 3: Inject the events into countdown.js
 let countdownJs = fs.readFileSync(path.resolve(__dirname, 'countdown.js'), 'utf-8');
-countdownJs = countdownJs.replace('// This will be populated by the build script', `// This will be populated by the build script\n${JSON.stringify(events)}`);
+countdownJs = countdownJs.replace('// This will be populated by the build script', `const events = ${JSON.stringify(events)};`);
 
 fs.writeFileSync(path.resolve(__dirname, 'countdown.js'), countdownJs);
